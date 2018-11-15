6c857226e6cb3b3e8a85a73ebeb1d24810e69e7e
Julius Petersson <42491392+KamikazeSloth@users.noreply.github.com>
2018-11-15 10:06:05 +0100

    Add files via upload

---
+++ QuoteProject/App.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="container">
+    <app-header :quoteCount="quotes.length" :maxQuotes="maxQuotes"></app-header>
+    <new-quote @quoteAdded="NewQuote"></new-quote> 
+    <quote-grid :quotes="quotes" @quoteDeleted="deleteQuote"></quote-grid>
+    <div class="row">
+        <div class="col-sm-12 text-center">
+            <div class="alert alert-info">Info: click on a quote to delete</div>
+        </div>
+    </div>
+    
+    </div>
+</template>
+
+<script>
+    import QuoteGrid from './components/QuoteGrid.vue'
+    import NewQuote from './components/NewQuote.vue'
+    import appHeader from './components/Header.vue'
+
+
+    export default {
+        data: function() {
+            return {
+                quotes: [
+                    'Just a Quote to see something'
+                ],
+                maxQuotes: 10
+            }
+        },
+        methods: {
+            NewQuote(quote) {
+                if (this.quotes.length >= this.maxQuotes){
+                    return alert('Can only add 10 quotes, delete previous quotes');
+                // }else if(Object.keys(this.quotes).length === 0){
+                //     return alert('The quote cannot be empty');
+                }else if(this.quotes.length <= this.maxQuotes){
+                    this.quotes.push(quote);
+                }
+            },
+            deleteQuote(index){
+                this.quotes.splice(index, 1);
+            }
+        },
+        components: {
+            QuoteGrid: QuoteGrid,
+            NewQuote: NewQuote,
+            appHeader: appHeader
+        }
+    }
+</script>
+
+<style>
+</style>
---
+++ QuoteProject/components/NewQuote.vue
@@ -0,0 +1,64 @@
+<template>
+    <div class="row">
+        <form>
+            <div class="col-sm-8 col-sm-offset-2 col-xs-12 col-md-6 col-md-offset3 form-group">
+                <label>Quote</label>
+                <textarea class="form-control" rows="3" v-model="quote">Write your quote here</textarea>
+        <div class="col-sm-8 col-sm-offset-2 col-xs-12 col-md-6 col-md-offset3 form-group">
+                <br>
+                <button @click.prevent="createNew">New quote</button>
+                </div>
+            </div>
+        </form>
+    </div>
+</template>
+
+<script>
+    import QuoteGridVue from './QuoteGrid.vue';
+
+    export default {
+        data: function() {
+            return {
+                quote: ''    
+            };
+        },
+        methods: {
+            createNew() {
+                this.$emit('quoteAdded', this.quote);
+                this.quote = '';
+            }
+        }
+    }
+</script>
+
+<style>
+    button {
+    background-color: rgb(69, 158, 170); /* Green */
+    border: none;
+    color: rgba(255, 255, 255, 0.904);
+    padding: 15px 32px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 20px;
+    }
+
+    button {
+    -webkit-transition-duration: 0.4s; /* Safari */
+    transition-duration: 0.4s;
+    }
+
+    button:hover {
+        background-color: rgba(83, 88, 86, 0.349); /* Green */
+        color: rgb(253, 252, 252);
+    }
+
+    button:active {
+    background-color: #3e8e41e0;
+    box-shadow: 0 5px rgb(255, 255, 255);
+    transform: translateY(10px);
+}
+
+</style>
+
+
